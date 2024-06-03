06112a4c61c72b0db10af87a12bf26a821a6cbef
Thakshaka Rathnayake <thakshakarathnayake15@gmail.com>
2024-06-04 02:40:54 +0530

    .

--- server/index.js
+++ server/index.js
@@ -29,7 +29,7 @@ app.post("/todos", async (req, res) => {
     const { data, error } = await supabase
       .from('todo')
       .insert([{ description }]);
-
+      
     if (error) throw error;
 
     if (data) {
